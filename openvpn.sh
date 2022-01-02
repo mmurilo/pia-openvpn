ab81e79e69666faa386a4e8bbdd9b62a2aeae003
Marcos M M Rocha <43329254+mmurilo@users.noreply.github.com>
2022-01-02 01:00:44 -0800

    adding entrypoint script

---
+++ openvpn.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e -u -o pipefail
+
+# These are the possible bundles from PIA
+# https://www.privateinternetaccess.com/openvpn/openvpn.zip
+# https://www.privateinternetaccess.com/openvpn/openvpn-strong.zip
+# https://www.privateinternetaccess.com/openvpn/openvpn-tcp.zip
+# https://www.privateinternetaccess.com/openvpn/openvpn-strong-tcp.zip
+
+baseURL="https://www.privateinternetaccess.com/openvpn"
+PIA_OPENVPN_CONFIG_BUNDLE=${PIA_OPENVPN_CONFIG_BUNDLE:-"openvpn"}
+VPN_PROVIDER_HOME=/pia
+
+# Download and extract wanted bundle into temporary file
+tmp_file=$(mktemp)
+echo "Downloading OpenVPN config bundle $PIA_OPENVPN_CONFIG_BUNDLE into temporary file $tmp_file"
+curl -sSL --cookie /dev/null "${baseURL}/${PIA_OPENVPN_CONFIG_BUNDLE}.zip" -o "$tmp_file"
+
+echo "Extract OpenVPN config bundle into PIA directory $VPN_PROVIDER_HOME"
+mkdir -p $VPN_PROVIDER_HOME
+unzip -qjo "$tmp_file" -d "$VPN_PROVIDER_HOME"
+
+set -- "$@" '--config' "${REGION:-US West}.ovpn"
+
+if [ ! -f auth.conf ]; then
+  echo "${USERNAME:-NONE PROVIDED}" > auth.conf
+  echo "${PASSWORD:-NONE PROVIDED}" >> auth.conf
+  chmod 600 auth.conf
+fi
+
+set -- "$@" '--auth-user-pass' 'auth.conf'
+
+openvpn "$@"
